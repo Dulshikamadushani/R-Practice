deacdd404d0ae299079883361b025a1cf19fdf1f
Dulshikamadushani <dmd0087@auburn.edu>
2026-02-12 09:01:21 -0600

    Assignment done

---
+++ Assignment_2_12_Final/Completed_Assignment_2_12/Dulshika_completed_Assignment.R
@@ -0,0 +1,37 @@
+# Explain the following concepts about ggplot 
+
+# a. What three elements do you need to produce a ggplot?
+# a dataset 
+# aesthetic mappings (aes)
+# geometric objects (geoms) 
+
+# b. What is a geom? 
+# A geom (geometric object) is the visual representation of data in a plot. Ex:
+# geom_boxplot() → boxplot
+# geom_bar() → bar chart
+# geom_point() → scatter plot
+
+# c. What is a facet? 
+# A facet splits a plot into multiple panels based on a categorical variable, therefore can compare groups easily.
+
+# d. Explain the concept of layering. 
+# Layering means building a plot by adding components one at a time using + operation.
+
+# e. Where do you add x and y variables and map different shapes, colors, and other attributes to the data? 
+# Inside the aes() function.
+
+
+
+library(ggplot2)
+library(readr)
+
+
+
+
+
+
+
+
+
+
+
